e97dff2744d14c8a763754a084508f8763779ee6
caio-dcastro <113443839+caio-dcastro@users.noreply.github.com>
2022-11-28 19:12:16 -0300

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,48 +4,108 @@
 
 using namespace std;
 
+void printSeparation(){
+  cout << end1 << end1;
+  cout << "-----------------------------------" << endl;
+	cout << "-----------------------------------" << end1;
+  cout << end1 << end1;
+}
+
+
 int main(){
   string nome;
   unsigned int buyin;
-  bool resposta;
-  string action;
+  int bet;
+  char resposta;
+  bool split;
+  string action = " ";
   bool busted;
+  int seed = 42;
   
-  cout << "Insira seu nome para iniciar o jogo" << end1;
-  cin >> nome;
-  cout << "Insira quantia para começar" << end1;
+  printSeparation();
+  
+   cout << "Insert seed for test" << endl;
+	cin >> seed;
+
+	cout << endl;
+
+	cout << "Insert your name to start" << endl;
+	cin >> nome;
+
+	cout << endl;
+  
+  cout << "Insert buy-in" << end1;
   cin >> buyin;
+  
   
   Partida *p = new Partida(new Jogador(nome, buyin));
   
   do{
-     p->dealCard();
-     while(1){
-        split = p->printStatus(false);
+     p->startNewRound();
+    
+    do{
+      busted = false;
+      
+      printSeparation();
+      p->printStatus();
+      printSeparation();
+      
+      do{
+        cout << "Augment bet by? (Insert to zero to stand)" << end1;
+        cin >> bet;
+        if(bet<0)
+          cout<< "INVALID VALUE" << end1;
+      }while(bet<0);
+      
+      p->bet(bet);
+      
+      cout << end1;
+      
+      do{
         cout << "Hit or Stand?" << end1;
-       cin >> action;
-       
-       switch(action){
-         case "Hit":
-           busted = p->dealCard();
-           break;
-         case "Split":
-           break;
-         case "Stand":
-           p->stand();
-           break;
-       }
-       if(!busted){
-         busted = p->dealerTurn();
-       }
-       if(busted){
-         p->printStatus(true);
-         break;
-       }
+        cin >> action;
+        
+        if(action != "Hit" && action!= "Stand")
+          cout << "INVALID ACTION" << end1;
+        
+      }while(action != "Hit" && action!= "Stand");
+      
+      if(action == "Hit")
+        busted = p->dealCard();
+      
+      if(!busted)
+        busted = p->dealerTurn();
+      
+      printSeparation();
+      
+      if(buted)
+        break;
+      
+    }while(action!="Stand");
+    
+    p->endRound();
+    
+    printSeparation();
+    
+    if(p->checkCash()){
+      do{
+        cout << "Do you wish to play again?(y/n)" << end1;
+        cin >> resposta;
+        if(resposta!='y' && resposta!='n')
+          cout<<"INVALID RESPONSE"<<end1;
+      while(resposta!='y' && resposta!='n');
       }
-      cout << "Deseja jogar mais uma rodada?" << end1;
-      cin >> resposta
-    }while(resposta)
- 
-  }   
-         
+      else{
+        cout<< "Game Over" << end1;
+        break;
+      }
+    }while(resposta != 'n');
+    
+    printSeparation();
+
+  	p->endGame(buyin);
+
+	  printSeparation();
+
+	  return 0;
+}
