2c738a271272065c1682235b96802bdd13fc66a5
Ervilha23 <108023597+Ervilha23@users.noreply.github.com>
2022-11-29 21:29:22 -0300

    Update Partida.cpp

--- Src/Partida.cpp
+++ Src/Partida.cpp
@@ -1,13 +1,21 @@
 #include "Partida.hpp"
 #include "Jogador.hpp"
 #include <string>
-#include <queue>
 #include <algorithm>
 #include <random>
 
-Partida::Partida(Jogador* jogador, int seed){
-    this->jogador=jogador;
-    this->dealer = new Jogador("dealer", -1);
+using namespace std;
+
+Partida::Partida(Jogador* jogador, int seed, int blind){
+    
+    this->blind = blind;
+    
+    this->jogadores[0] = new Jogador("Dealer", -1;
+    this->jogadores[1] = jogador;
+    
+    this->jogador = this->jogadores[1];
+    this->dealer = this->jogadores[0];
+                                     
     this->initializeDeck(seed);
 }
 
@@ -17,56 +25,64 @@ Partida::~Partida(){
 
 void Partida::initializeDeck(int seed){
     for (int deckqnt = 0; deckqnt<2; deckqnt++){
-        for(int i = 1; i < 14; i++){
+        for(int i = 2; i < 14; i++){
             for(int j = 0; j < 4; j++){
                 deck.push_back(Carta(i,j));
             }
         }
     }
-    shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));
+    shuffle(deck.begin(), deck.end(), default_random_engine(seed));
 }
 void Partida::startNewRound(){
-    jogador->bet(baseBet);
+    jogador->bet(blind);
+    
+    dealCard(jogador->getName());
+    dealCard(dealer->getName());
+    dealCard(jogador->getName());
+    dealCard(dealer->getName());
+}
+                                     
+bool Partida::dealCard(string playersName){
+    int i = 0;
+    for(; i<jogadores.size(); i++){
+        if(jogadores[i]->getName() == playersName)
+            break;
+    }
+    if(i == jogadores.size())
+        exit(1);
+    
     Carta carta = deck.front();
     deck.pop_front();
-    jogador->adicionaCarta(carta);
-    carta = deck.front();
-    deck.pop_front();
-    dealer->adicionaCarta(carta);
-    carta = deck.front();
-    deck.pop_front();
-    jogador->adicionaCarta(carta);
-    carta = deck.front();
-    deck.pop_front();
-    dealer->adicionaCarta(carta);
-};
-bool Partida::dealCard(){
-    Carta carta = deck.front();
-    deck.pop_front();
-    return jogador->adicionaCarta(carta);
-};
+    return jogadores[i]->adicionaCarta(carta);
+}
+                                     
 bool Partida::dealerTurn(){
     int dealerSum = dealer->getSum();
+    
     int jogadorSum = jogador->getSum();
+    
     while(dealerSum < jogadorSum){
-        Carta carta = deck.front();
-        deck.pop_front();
-        dealer->adicionaCarta(carta);
+        
+        dealCard(dealer->getName());
         dealerSum = dealer->getSum();
     }
     return dealerSum > 21;
-};
+}
 void Partida::printStatus(){
+    
     jogador->imprimirStatus();
+    count << end1;
     dealer->imprimirStatus();
-};
-bool Partida::bet(int valor){
+}
+void Partida::bet(int valor){
     return jogador->bet(valor);
 }
 void Partida::endRound(){
+    
     int jogadorPontos = jogador->imprimirStatus();
     int dealerPontos = dealer->imprimirStatus();
     int gameResult = 0;
+    
     if(jogadorPontos==dealerPontos){
         cout << "Draw!" << endl;
         gameResult = 1;
@@ -77,7 +93,7 @@ void Partida::endRound(){
     }
     else{
         cout << "Dealer wins!" << endl;
-        gameResult = -1;
+        gameResult = 0;
     }
     while(jogador->getNumCards()){
         deck.push_back(jogador->popCard());
@@ -86,7 +102,13 @@ void Partida::endRound(){
         deck.push_back(dealer->popCard());
     }
     jogador->endRound(gameResult);
-};
+}
+                                     
 bool Partida::checkCash(){
     return jogador->checkCash();
+}
+void Partida::endGame(int buyin){
+    cout<< "Entered with: " << buyin << endl;
+	cout<< "Left with: " <<  jogador->getDinheiro() << endl;
+    cout<< "Profit = " << jogador->getDinheiro() - buyin;
 }
